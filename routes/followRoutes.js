1ac755b85336c57229dabd6daefaa59901376ca7
ivaanrl <ivanrl2010@gmail.com>
2020-01-22 18:46:21 -0300

    follow/unfollow works. Need to implement initial loading of follow state

--- routes/followRoutes.js
+++ routes/followRoutes.js
@@ -1,24 +1,19 @@
 const uuid = require('uuid');
 const sequelize = require('../config/postgres.config');
 //const Follows = require('../models/follows');
+const User = require('../models/user');
 
 module.exports = app => {
   app.post('/api/followers/follow', async (req, res) => {
     try {
       console.log('Followinnnnnnnnnnnnnnnng');
       const usersIds = await sequelize.query(
-        `SELECT id FROM users WHERE username='${req.body.follower}' OR username='${req.body.following}'`
+        `INSERT INTO follows 
+        VALUES ('${uuid()}',( SELECT id FROM users WHERE username='${
+          req.body.follower
+        }'), (SELECT id FROM users WHERE username='${req.body.following}'))`
       );
-      console.log('Followinnnnnnnnnnnnnnnng');
-      const id = uuid();
-      const [user_id, follower_id] = usersIds[0];
-      console.log(user_id);
-      const createFollow = await sequelize.query(
-        `INSERT INTO follows (id,user_id,follower_id)
-         VALUES ${id}, ${user_id.id}, ${follower_id.id}
-        `
-      );
-      res.json(createFollow);
+      res.json('Success!');
     } catch (error) {
       res.json(error);
     }
@@ -27,17 +22,16 @@ module.exports = app => {
   app.post('/api/followers/unfollow', async (req, res) => {
     try {
       const usersIds = await sequelize.query(
-        `SELECT id FROM users WHERE username='${req.body.follower}' OR username='${req.body.following}'`
+        `DELETE FROM follows 
+        WHERE user_id= (
+                        SELECT id FROM users WHERE username='${req.body.follower}'
+                       ) AND
+            follower_id = (
+                          SELECT id FROM users WHERE username='${req.body.following}'
+                          )`
       );
-      const [user_id, follower_id] = usersIds[0];
-      const destroyFollow = await Follows.destroy({
-        where: {
-          user_id,
-          follower_id
-        }
-      });
-      console.log(destroyFollow);
-      res.json(req.body);
+      console.log('akjbdaslkjdk');
+      res.json('res');
     } catch (error) {
       res.json(error);
     }
